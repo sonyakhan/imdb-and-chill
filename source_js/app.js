88fa0592e1b6c2d70078a1a5faf564127db335b3
Sonya Khan <sonya.khan.cs@gmail.com>
2016-10-18 20:51:30 -0500

    Final commit

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,27 @@
+/* Module */
 var app = angular.module('mp3',['ngRoute']);
 
+/* Routes */
 app.config(function ($routeProvider) {
 
-})
+    $routeProvider
+
+        .when('/', {
+        templateUrl: './partials/list.html',
+        controller: 'listController'
+        })
+
+        .when('/gallery', {
+            templateUrl: './partials/gallery.html',
+            controller: 'galleryController'
+        })
+
+        .when('/list/:imdbID/:rank', {
+            templateUrl: './partials/details.html',
+            controller: 'detailsController'
+        })
+        .otherwise({
+            redirectTo: '/list'
+        });
+});
+
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,40 @@
-/* Sample Controller */
+/* Details Controller */
+app.controller('detailsController', ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http) {
+
+        $scope.imdbID = $routeParams.imdbID;
+        $scope.rank = $routeParams.rank;
+        $scope.title = $routeParams.title;
+        $scope.prev = $scope.rank - 1;
+        $scope.next = $scope.rank - (-1);
+
+        $http.get('./data/imdb250.json').success(function(response) {
+            $scope.movies = response;
+        });
+}]);
+
+/* Gallery Controller */
+app.controller('galleryController', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.genreFilter = '';
+    $http.get('./data/imdb250.json').success(function(response) {
+        $scope.movies = response;
+    });
+}]);
+
+/* List Controller */
+app.controller('listController', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.sort = 'rank';
+    $scope.movies = [];
+    $scope.filterOptions = [
+            {id: 1, name: 'Rank'},
+            {id: 2, name: 'Title'}
+    ];
+    $http.get('./data/imdb250.json')
+        .success(function (response) {
+            $scope.movies = response;
+        })
+        .error(function (err) {
+            console.log(err);
+        })
+}]);
